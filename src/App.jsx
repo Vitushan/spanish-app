1787409d3aad58cc4a254fd685aca7adae685bae
Vitushan <vitushan@outlook.fr>
2025-12-09 18:29:55 -0500

    configurated tailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-red-500'>My Learning Spanish App</h1>
+    </>
+  )
+}
+
+export default App
