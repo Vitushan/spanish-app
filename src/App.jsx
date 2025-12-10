bfd50d2d9973eb882ae3103d0fd0206c6841cec3
Vitushan <vitushan@outlook.fr>
2025-12-09 19:15:34 -0500

    layout header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
-
+// Un composant React = une fonction
   return (
     <>
+      <Header /> {/* utilisation du composant Header */}
       <h1 className='text-3xl font-bold text-red-500'>My Learning Spanish App</h1>
     </>
   )
